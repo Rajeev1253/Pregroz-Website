9d875d1428a14834f40203c224727820e048800f
Rajeev1253 <3241rajeev@gmail.com>
2024-01-16 11:29:56 +0530

    fourth commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function showSidebar() {
+    const sidebar = document.querySelector(".mobile-nav");
+    sidebar.style.display = "flex";
+  }
+  function hideSidebar() {
+    const sidebar = document.querySelector(".mobile-nav");
+    sidebar.style.display = "none";
+  }
+  const submit= document.getElementById('login');
+  submit.addEventListener("click",()=>{
+    const email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    const em = localStorage.setItem("email",email);
+    let pass = localStorage.setItem("password",password);
+
+  })
